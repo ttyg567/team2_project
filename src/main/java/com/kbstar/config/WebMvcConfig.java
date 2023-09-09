a21f1ef4c8a3ed6ef2b97e8ea74f02ed1391b9ec
Jeeyei Oh <ttyg567@naver.com>
2023-09-09 15:05:24 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.kbstar.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 이 경로에 있는 폴더를 static에 있는 폴더처럼 Web에서 활용할 수 있도록 함
+    @Value("${imgdir}")     // spring 으로 import
+    String imgdir;
+    @Value("${logdir}")
+    String logdir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uimg/**").addResourceLocations(imgdir);
+        registry.addResourceHandler("/logs/**").addResourceLocations(logdir);
+    }
+
+}
---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.kbstar.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        return "index";
+    }
+
+}
